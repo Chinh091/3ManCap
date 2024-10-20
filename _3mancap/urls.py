aebb336c584298ff88052cddff6f28f76e79c6f4
Chinh091 <164608846+Chinh091@users.noreply.github.com>
2024-10-20 18:52:42 +1100

    Update urls.py

--- _3mancap/urls.py
+++ _3mancap/urls.py
@@ -2,7 +2,11 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='home'),  # Dashboard as the home page
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
